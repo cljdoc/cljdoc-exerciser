a1cfe14bb017fd1f1acbadec1626a472993248a2
lread <lee@dlread.com>
2022-02-16 15:37:19 -0500

    Add in test for replanted/relative wikilink

--- src/cljdoc_exerciser/ns2.clj
+++ src/cljdoc_exerciser/ns2.clj
@@ -1,5 +1,13 @@
 (ns cljdoc-exerciser.ns2)
 
 (defn whatever
-  "Another ns another var"
+  "Another ns another var
+
+  Here we demonstrate namespace relative wikilinking, these should be eqivalent:
+  - [[ns3/otherthing]]
+  - [[cljdoc-exerciser.ns3/otherthing]]
+
+  I personally don't find this the most intuitive scheme, I would have thought
+  the relative syntax would bring me to `cljdoc-exerciser.ns2.ns3/otherthing`, but
+  so be it."
   [x] x)
---
+++ src/cljdoc_exerciser/ns3.clj
@@ -0,0 +1,5 @@
+(ns cljdoc-exerciser.ns3)
+
+(defn otherthing
+  "Testing 4 5 6."
+  [x y])
