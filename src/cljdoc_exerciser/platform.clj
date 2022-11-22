2004b009df068565b4716d12fe38cbf103407f9f
lread <lee@dlread.com>
2022-11-22 11:43:42 -0500

    Explore how API platform differences are rendered

---
+++ src/cljdoc_exerciser/platform.clj
@@ -0,0 +1,27 @@
+(ns cljdoc-exerciser.platform
+  "Clojure namespace - test when there are differences between platforms.")
+
+(defn same "Docstring matches for all platforms, but links to sources should be different" [a])
+
+(defn docs-differ "Clojure specific docstring here" [a])
+
+(defn args-differ "Docstrings same, but arglists differ" [a])
+
+(defn docs-and-args-differ "Clojure specific docstring here, args differ too" [a])
+
+(defprotocol ProtoAllSame
+  "Protocol with matching everything"
+  (proto1-same1 [a b] "docstring for proto1-same1")
+  (proto1-same2 [a] [a b] [a b c] "docstring for proto1-same2"))
+
+(defprotocol ProtoDocsDiffer
+  "Protocol with Clojure specific docstring"
+  (proto2-same1 [a b] "docstring for proto2-same1")
+  (proto2-same2 [a] [a b] [a b c] "docstring for proto2-same2") )
+
+(defprotocol ProtoDocsAndMembersDiffer
+  "Protocol with Clojure specific docstring"
+  (proto3-clj-only [a b] "docstring for proto3-clj-only")
+  (proto3-docstrings-differ [a b] "clojure specific docstring for proto3-docstrings-differ")
+  (proto3-args-differ [a b] "dosctrings same, arglists differ for proto3-args-differ")
+  (proto3-same [a] [a b] [a b c] "docstring for proto3-same"))
